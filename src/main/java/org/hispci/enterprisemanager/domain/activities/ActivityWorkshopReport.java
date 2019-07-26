690adc9592a8414027d066745489240ad8bed0e5
spbogui <spbogui@gmail.com>
2019-07-26 15:04:31 +0000

    add activity type rest resource

--- src/main/java/org/hispci/enterprisemanager/domain/activities/ActivityWorkshopReport.java
+++
@@ -1,7 +0,0 @@
-package org.hispci.enterprisemanager.domain.activities;
-
-import javax.persistence.Entity;
-
-@Entity
-public class ActivityWorkshopReport extends ActivityReport {
-}
--- src/main/java/org/hispci/enterprisemanager/repositories/activities/ActivitySupervisionReportRepository.java
+++
@@ -1,9 +0,0 @@
-package org.hispci.enterprisemanager.repositories.activities;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface ActivitySupervisionReportRepository extends JpaRepository<ActivitySupervisionReport, Long> {
-}
